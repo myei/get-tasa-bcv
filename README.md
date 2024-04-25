c242546e465b9d2a65ad96c1976da69324d809f0
Manuel Gil <mgil@initiumsoft.com>
2024-04-25 13:23:58 -0400

    agregando capacidad de especifcar navegador (firefox, chrome)

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # get-tasa-bcv.py
 
-Ejemplo de ejecución:
+> ## Requisitos:
+> - Tener instalado Python <= 3.9
+> - Descargar el  [driver](https://selenium-python.readthedocs.io/installation.html#drivers) del navegador a utilizar y añadirlo al ```$PATH```
+> - Tener instalado [pip](https://pip.pypa.io/en/stable/installation/)
+> - Instalar las dependencias del script: (```pip3.9 install -r requirements.txt```)
+
+## Ejemplo de ejecución:
 
 ```shell
-pip3.9 install -r requirements.txt # installing dependencies
+python3.9 get-tasa-bcv.py # Utilizara chrome por defecto
 
-python3.9 get-tasa-bcv.py
+python3.9 get-tasa-bcv.py -f # Utilizando firefox
+python3.9 get-tasa-bcv.py -c # Utilizando firefox
 ```
--- get-tasa-bcv.py
+++ get-tasa-bcv.py
@@ -1,32 +1,54 @@
+#!/usr/bin/env python3.9 -u
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from locale import setlocale, atof, LC_ALL
+from sys import argv
+
 
 PARAMETERS = {
     "bcv_url": "https://www.bcv.org.ve/",
     "rate_field": "dolar",
     "browser": {
+        "drivers": [webdriver.Chrome, webdriver.Firefox],
+        "optionsHandler": [ChromeOptions, FirefoxOptions],
         "size": "--window-size=1920,1080",
         "mode": "--private-window",
-        "visibility": "--headless"
+        "visibility": "--headless",
+        "availables": {
+            "-c": 0,
+            "-f": 1 
+        }
     }
-
 }
 
+DRIVER_TO_USE = 0
+if len(argv) > 1 and argv[1] in list(PARAMETERS["browser"]["availables"].keys()):
+    DRIVER_TO_USE = PARAMETERS["browser"]["availables"][argv[1]]
 
-setlocale(LC_ALL, '')
+print("Utilizando Firefox..." if DRIVER_TO_USE == 1 else "Utilizando chrome...")
 
-options = Options()
-options.add_argument(PARAMETERS['browser']['size'])
-options.add_argument(PARAMETERS['browser']['mode'])
-options.add_argument(PARAMETERS['browser']['visibility'])
+print("Consultando tasa de cambio en: ", PARAMETERS["bcv_url"])
 
-driver = webdriver.Chrome(options=options)
-driver.get(PARAMETERS['bcv_url'])
+setlocale(LC_ALL, "")
 
-target = driver.find_element(By.ID, PARAMETERS['rate_field'])
-rate = round(atof((target.text.split('\n')[1])), 2)
+options = PARAMETERS["browser"]["optionsHandler"][DRIVER_TO_USE]()
+options.add_argument(PARAMETERS["browser"]["visibility"])
+
+if DRIVER_TO_USE == PARAMETERS["browser"]["availables"]["-c"]:
+    options.add_argument(PARAMETERS["browser"]["size"])
+    options.add_argument(PARAMETERS["browser"]["mode"])
+else:
+    options.add_argument("--width=2560")
+    options.add_argument("--height=1440")
+
+driver = PARAMETERS["browser"]["drivers"][DRIVER_TO_USE](options=options)
+driver.get(PARAMETERS["bcv_url"])
+
+target = driver.find_element(By.ID, PARAMETERS["rate_field"])
+rate = round(atof((target.text.split("\n")[1])), 2)
 
 print("Tasa $:", rate)
 
